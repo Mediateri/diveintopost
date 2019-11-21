5c8632e2bda2d9eaed27b70b6877d2ff1fd9ea2c
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-11-21 07:36:32 +0000

    myLast

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -4,7 +4,7 @@ class AgendaMailer < ApplicationMailer
         @agenda = agenda
         team = agenda.team
         team.assigns.each do |assign|
-            mail to: assign.email, subject: 'Agenda was deleted'
+            mail to: assign, subject: 'Agenda was deleted'
         end
 
     end
