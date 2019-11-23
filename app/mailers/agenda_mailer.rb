9d86a554e604542b56f3b7a3d360a8ebcef49922
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-11-23 19:36:31 +0000

    me

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -2,7 +2,7 @@ class AgendaMailer < ApplicationMailer
     default from: 'from@example.com'
     def agenda_mail(agenda)
         @agenda = agenda
-        @agenda.team.assigns.seach do |assign|
+        @agenda.team.assigns.each do |assign|
             mail to: assign.user.email ,  suject: "Agenda deleted"
         end
     end
